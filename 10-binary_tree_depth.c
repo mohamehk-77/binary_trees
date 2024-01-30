3757dd8c70beb26b100e17bd380be5cb9c48dc50
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-01-30 23:02:09 +0200

    File

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -4,20 +4,22 @@
  * binary_tree_depth - function that count the depth of node
  *
  * @tree: parameter that point to binary tree
+ *
+ * Return: Depth Of The Node
 */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    size_t depth;
+	size_t depth;
 
-    depth = 0;
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    while (tree->parent != NULL)
-    {
-        depth++;
-        tree = tree->parent;
-    }
-    return depth;
+	depth = 0;
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	while (tree->parent != NULL)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+	return (depth);
 }
