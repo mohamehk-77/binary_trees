df1984b970ee83675dfc80deaa5158704b2641da
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-01-30 21:57:40 +0200

    Betty Edit

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -22,7 +22,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
-	if (parent-> left != NULL)
+	if (parent->left != NULL)
 	{
 		NewNode->left = parent->left;
 		parent->left->parent = NewNode;
