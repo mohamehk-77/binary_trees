7187891f14a1e8cc6243489a23976ff8df7ab04c
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-01-31 01:04:32 +0200

    Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,14 +13,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t l_height = 0;
 	size_t r_height = 0;
 
-    if (tree == NULL)
-    {
-        return 0;
-    }
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	l_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	r_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-    l_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-    r_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-
-    return (l_height > r_height) ? l_height : r_height;
+	return ((l_height > r_height) ? l_height : r_height);
 }
 
