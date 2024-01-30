da34741ff3fa9f071531677ab32ac5430a052563
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-01-31 00:47:28 +0200

    Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,23 +1,24 @@
 #include "binary_trees.h"
 
 /**
- * 
- * 
- * 
- * 
- * 
+ * binary_tree_height - function that measures the
+ * height of a binary tree
+ *
+ * @tree: parameter that point to binary tree
+ *
+ * Return: Zero if it fails
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t l_height;
-    size_t r_height;
+	size_t l_height;
+	size_t r_height;
 
-    if (NULL == tree)
-    {
-        return (0);
-    }
-    l_height = binary_tree_height(tree->left);
-    r_height = binary_tree_height(tree->right);
+	if (NULL == tree)
+	{
+		return (0);
+	}
+	l_height = binary_tree_height(tree->left);
+	r_height = binary_tree_height(tree->right);
 
-    return(l_height > r_height ? l_height : r_height) + 1;
+	return(l_height > r_height ? l_height : r_height) + 1;
 }
