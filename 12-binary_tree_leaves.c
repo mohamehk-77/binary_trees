08478928b8659c959339a1d0fe7f61c6cb19a127
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-01-31 20:53:04 +0200

    Leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -5,19 +5,20 @@
  * leaves in a binary tree
  *
  * @tree: pointer to the root node of the tree to
- * count the number of leaves 
+ * count the number of leaves
  *
  * Return: The number of leaves in the tree or -1 if the tree is empty
 */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    if (tree->right == NULL && tree->left == NULL)
-    {
-        return (1);
-    }
-    return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->right == NULL && tree->left == NULL)
+	{
+		return (1);
+	}
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
+
